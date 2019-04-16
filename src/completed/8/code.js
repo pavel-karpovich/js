e39e3cbe0d4447f53deaa6fcd73273a240d41b58
Pavel <Chesh397@mail.ru>
2019-04-16 21:37:56 +0300

    Minesweeper v0.1

--- src/completed/8/code.js
+++ src/completed/8/code.js
@@ -2,8 +2,12 @@
 let menu = document.querySelector(".menu");
 let menuButton = document.querySelector(".header > button");
 
+function closeMenu() {
+    menu.classList.add("menu-hidden");
+    menuButton.classList.remove("is-active");
+}
+
 document.addEventListener("keydown", function(e) {
-    console.log(e);
     if (e.key === "Escape") {
         menu.classList.toggle("menu-hidden");
         menuButton.classList.toggle("is-active");
@@ -11,10 +15,9 @@ document.addEventListener("keydown", function(e) {
 
 });
 
-let closeMenu = document.querySelector(".menu-close");
-closeMenu.addEventListener("click", function() {
-    menu.classList.add("menu-hidden");
-    menuButton.classList.remove("is-active");
+let closeMenuButton = document.querySelector(".menu-close");
+closeMenuButton.addEventListener("click", function() {
+    closeMenu();
 });
 
 menuButton.addEventListener("click", function() {
@@ -27,6 +30,9 @@ class MinesweeperGame {
 
     constructor(xSize, ySize, bombNumber) {
 
+        if (bombNumber > xSize * ySize) {
+            bombNumber = 0;
+        }
         this.x = xSize;
         this.y = ySize;
         this.totalBombs = bombNumber;
@@ -41,15 +47,15 @@ class MinesweeperGame {
         }
         let bombPositions = [];
         const totalCells = this.x * this.y;
-        do {
-
+        
+        while (bombPositions.length != this.totalBombs)
+        {
             let rand = Math.floor(Math.random() * totalCells);
             if (!bombPositions.includes(rand)) {
                 bombPositions.push(rand);
                 this.board[Math.floor(rand / this.x)][rand % this.y].hasBomb = true;
             }
-
-        } while (bombPositions.length != this.totalBombs);
+        }
     }
 
     pickCell(x, y) {
@@ -58,11 +64,17 @@ class MinesweeperGame {
         if (this.board[y][x].hasBomb) {
             return "BOOOM!";
         }
-        const comb = [-1, 0, 1];
+        const comb = [0, -1, 1];
         const combCount = comb.length * comb.length;
         let sum = 0;
-        for (let i = 0; i < combCount; ++i) {
-            let neighboringCell = this.board[y + comb[Math.floor(i / comb.length)]][x + comb[i % comb.length]];
+        for (let i = 1; i < combCount; ++i) {
+
+            let yIndex = y + comb[Math.floor(i / comb.length)];
+            let xIndex = x + comb[i % comb.length];
+            if (!this.isCellReal(xIndex, yIndex)) {
+                continue;
+            }
+            let neighboringCell = this.board[yIndex][xIndex];
             if (neighboringCell.hasBomb) {
                 ++sum;
             }
@@ -77,13 +89,23 @@ class MinesweeperGame {
         this.foundBombs++;
     }
 
+    isCellReal(x, y) {
+
+        if (x < 0 || x >= this.x || y < 0 || y >= this.y) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
 }
 
 
 let game = null;
 
+let gameBoardContainer = document.querySelector(".board-container");
 let gameBoard = document.querySelector(".board");
+let gameBoardText = document.querySelector(".game > span");
 
 let startButton = document.querySelector(".menu-content > button");
 startButton.addEventListener("click", function() {
@@ -92,18 +114,75 @@ startButton.addEventListener("click", function() {
     let ySize = parseInt(document.getElementsByName("area-height")[0].value);
     let bombs = parseInt(document.getElementsByName("bomb-number")[0].value);
     game = new MinesweeperGame(xSize, ySize, bombs);
-    for (let row of game.board) {
+    if (gameBoardContainer.classList.contains("invisible")) {
+        gameBoardText.classList.add("invisible");
+        gameBoardContainer.classList.remove("invisible");
+    }
+    for (let i = 0; i < game.board.length; ++i) {
 
+        let row = game.board[i];
         let rowDiv = document.createElement("div");
         gameBoard.appendChild(rowDiv);
-        for (let cell of row) {
+        for (let j = 0; j < row.length; ++j) {
 
+            let cell = row[j];
             let cellDiv = document.createElement("div");
             cellDiv.classList.add("cell");
             cellDiv.textContent = "?";
+            cellDiv.dataset.x = j;
+            cellDiv.dataset.y = i;
             rowDiv.appendChild(cellDiv);
 
         }
         
     }
+    gameBoard.addEventListener("click", function(e) {
+        
+        if (!e.target.classList.contains("cell")) {
+            return;
+        }
+        let cell = e.target;
+        let result = game.pickCell(parseInt(cell.dataset.x), parseInt(cell.dataset.y));
+        if (result === "BOOOM!") {
+            cell.classList.add("bomb");
+            cell.textContent = "💣";
+            return;
+        }
+
+        function revealNeighboars(centerCell) {
+
+            let comb = [0, -1, 1];
+            let combCount = comb.length * comb.length;
+            for (let i = 1; i < combCount; ++i) {
+
+                let yIndex = parseInt(centerCell.dataset.y) + comb[Math.floor(i / comb.length)];
+                let xIndex = parseInt(centerCell.dataset.x) + comb[i % comb.length];
+                if (!game.isCellReal(xIndex, yIndex)) {
+                    continue;
+                }
+                let neighboringCell = gameBoard.children[yIndex].children[xIndex];
+                if (neighboringCell.className != "cell") {
+                    continue;
+                }
+                let result = game.pickCell(parseInt(neighboringCell.dataset.x), parseInt(neighboringCell.dataset.y));
+                neighboringCell.classList.add("safely");
+                if (result === 0) {
+                    neighboringCell.textContent = "";
+                    revealNeighboars(neighboringCell);
+                } else {
+                    neighboringCell.textContent = result;
+                }  
+            }
+        }
+
+        cell.classList.add("safely");
+        if (result === 0) {
+            cell.textContent = "";
+            revealNeighboars(cell);
+        } else {
+            cell.textContent = result;
+        }
+
+    });
+    closeMenu();
 });
